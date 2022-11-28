440a46d62c2ea5d9291723d36af85e6e7e837638
Harry <harrylin349@gmail.com>
2022-11-27 20:16:32 -0500

    search insert position sol

---
+++ Algorithms/35. Search Insert Position/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums)
+        mid = floor((left + right) / 2)
+        while ( left < right):
+            if (nums[mid] == target):
+                return mid
+            elif (nums[mid] < target):
+                left = mid + 1
+            else:
+                right = mid
+            mid = floor((left + right) / 2) 
+        return mid
