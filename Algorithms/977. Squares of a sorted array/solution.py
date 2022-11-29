7f8ecad92207ce524daba03b9b023e47e8db6dda
Harry <harrylin349@gmail.com>
2022-11-28 20:51:58 -0500

    add sol to 977. squares of a sorted array

---
+++ Algorithms/977. Squares of a sorted array/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        positives = []
+        negatives = []
+        sortedNums = []
+        lnum = len(nums)
+        for num in nums:
+            if num < 0:
+                negatives.insert(0, pow(num,2))
+            else:
+                positives.append(pow(num,2))
+        while (len(positives) > 0 and len(negatives) > 0):
+            if (positives[0] < negatives[0]):
+                sortedNums.append(positives.pop(0))
+            else:
+                sortedNums.append(negatives.pop(0))
+        if (len(positives) > 0):
+            sortedNums.extend(positives)
+        else:
+            sortedNums.extend(negatives)
+        return sortedNums
