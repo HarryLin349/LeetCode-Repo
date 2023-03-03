62bc474533e287a13ec985a1b200b6830223bfde
Harry <harrylin349@gmail.com>
2023-03-03 01:11:32 -0500

    java solutions for array hashing problem

---
+++ Algorithms/Arrays + Hashing/[E] 217. Contains Duplicate/setsolution.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>(nums.length);
+        for (int x: nums) {
+            if (set.contains(x)) return true;
+            set.add(x);
+        }
+        return false;
+    }
+}
