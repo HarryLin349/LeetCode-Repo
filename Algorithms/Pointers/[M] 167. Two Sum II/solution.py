585465b420ea5b53019b0f0afa8b50a4b107c46a
Harry <harrylin349@gmail.com>
2023-06-05 23:32:56 -0400

    167. Two Sum II

---
+++ Algorithms/Pointers/[M] 167. Two Sum II/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i1 = 1
+        i2 = len(numbers)
+        while (i1 < i2):
+            sum = numbers[i1-1] + numbers[i2-1]
+            if sum == target:
+                return [i1,i2]
+            elif sum < target:
+                i1 += 1
+            elif sum > target:
+                i2 -= 1
