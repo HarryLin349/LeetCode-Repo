1125f26f678770b4139171bf17e9e8842e7b8e97
Harry <harrylin349@gmail.com>
2023-06-06 20:25:49 -0400

    15. 3Sum

---
+++ Algorithms/Pointers/[M] 15. 3Sum/slowsolution.py
@@ -0,0 +1,30 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        print(nums)
+        answer = []
+        for ind, num in enumerate(nums):
+            result = self.twoSum(nums, num * -1, ind)
+            if len(result) == 0:
+                continue
+            for ans in result:
+                ans.append(num)
+                if sorted(ans) not in answer:
+                    answer.append(sorted(ans))
+        return answer
+    
+    def twoSum(self, numbers: List[int], target: int, forbidden: int) -> List[int]:
+        i1 = 0
+        i2 = len(numbers) - 1
+        answer = []
+        while (i1 < i2):
+            sum = numbers[i1] + numbers[i2]
+            if sum == target and i1 != forbidden and i2 != forbidden:
+                answer.append([numbers[i1],numbers[i2]])
+                i1 += 1
+            elif sum < target:
+                i1 += 1
+            else:
+                i2 -= 1
+        return answer
+    
---
+++ Algorithms/Pointers/[M] 15. 3Sum/solmod2sum.py
@@ -0,0 +1,37 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        print(nums)
+        answer = []
+        for ind, num in enumerate(nums):
+            if ind > 0 and nums[ind] == nums[ind - 1]:
+                continue
+            result = self.twoSum(nums[ind + 1:], num * -1)
+            if len(result) == 0:
+                continue
+            for ans in result:
+                if ans[0] > num:
+                    ans.insert(0,num)
+                elif ans[1] > num:
+                    ans.insert(1,num)
+                else:
+                    ans.append(num)
+                if ans not in answer:
+                    answer.append(ans)
+        return answer
+    
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i1 = 0
+        i2 = len(numbers) - 1
+        answer = []
+        while (i1 < i2):
+            sum = numbers[i1] + numbers[i2]
+            if sum == target:
+                answer.append([numbers[i1],numbers[i2]])
+                i1 += 1
+            elif sum < target:
+                i1 += 1
+            else:
+                i2 -= 1
+        return answer
+    
