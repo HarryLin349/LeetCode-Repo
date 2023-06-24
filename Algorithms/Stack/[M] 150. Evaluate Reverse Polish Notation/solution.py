7b696deb80a47099652d16e8ad1500d7b77f668c
Harry <harrylin349@gmail.com>
2023-06-23 20:53:53 -0400

    150. Evaluate Reverse Polish Notation

---
+++ Algorithms/Stack/[M] 150. Evaluate Reverse Polish Notation/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        ops = ["+","-","/","*",]
+        stack = []
+        first = True
+        for s in tokens:
+            if s in ops:
+                a2, a1 = stack.pop(), stack.pop()
+                if s == "+":
+                    stack.append(a1 + a2)
+                elif s == "-":
+                    stack.append(a1 - a2)
+                elif s == "*":
+                    stack.append(a1 * a2)
+                else:
+                    stack.append(int(a1 / a2))
+            else:
+                stack.append(int(s))
+        return stack[-1]
