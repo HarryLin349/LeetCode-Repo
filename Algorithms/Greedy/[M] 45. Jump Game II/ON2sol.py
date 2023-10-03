dbb03d7e1ae21ed09271f9ed141299c5a771cf7b
Harry Lin <harrylin349@gmail.com>
2023-10-03 17:28:27 +0800

    Jump game II (Greedy)

---
+++ Algorithms/Greedy/[M] 45. Jump Game II/ON2sol.py
@@ -0,0 +1,13 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        jumps = [-1] * n
+        jumps[n-1] = 0
+        for i in reversed(range(n -1)):
+            cmin = jumps[i+1]
+            indmax = min(i+nums[i], n-1)
+            for j in range(i + 1, indmax + 1):
+                cmin = min(cmin, jumps[j])
+            jumps[i] = cmin + 1
+        print(jumps)
+        return jumps[0]
