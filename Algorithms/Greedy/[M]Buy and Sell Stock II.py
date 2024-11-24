01934b72a39bd9e14194b531ad6fe3acbf043860
Harry <harrylin349@gmail.com>
2024-11-24 17:18:55 -0500

    Greedy, sliding window

---
+++ Algorithms/Greedy/[M]Buy and Sell Stock II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # idea: we buy at each low and sell at each high
+        # maxprofit -> we only profit at each rise -> we only care when stock goes up
+        # i.e. at each step if the stock went up, we buy 
+        profit = 0
+        prev = prices[0]
+        for i in range(len(prices)):
+            if prev < prices[i]:
+                profit += prices[i] - prev
+            prev = prices[i]
+        return profit
+            
