0e1facc73a47c16b56f87423d214ea333ee3a372
Harry <harrylin349@gmail.com>
2025-01-30 14:25:10 -0500

    309. Best Time to Buy and Sell Stock with Cooldown

---
+++ Algorithms/2D DP/[M] Buy and Sell Stocks with Cooldown.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        #idea: dp
+        # dp[i][j] = most profit from day i..n given you can buy, with j being 0 or 1 = own or not
+        # can buy, sell (skip next day), or hold
+        n = len(prices)
+        memo = [[-1 for _ in range(2)] for _ in range(n)] 
+        def dp(i,j):
+            if i >= n:
+                return 0
+            if memo[i][j] != -1:
+                return memo[i][j]
+            opt1, opt2, opt3 = -1, -1, -1
+            # case 1: buy
+            if j == 0:
+                opt1 = dp(i + 1, 1) - prices[i]
+            # case 2: sell
+            else:
+                opt2 = prices[i] + dp(i + 2, 0)
+            # case 3: wait/hold
+            opt3 = dp(i+1, j)
+            result = max(opt1, opt2, opt3)
+            memo[i][j] = result
+            return result
+        return dp(0,0)
+    
