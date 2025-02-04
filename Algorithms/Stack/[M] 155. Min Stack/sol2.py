5a769eee7c91a316ea6c3bc1f2e3d9c684c1fba4
Harry <harrylin349@gmail.com>
2025-02-04 12:41:40 -0500

    minstack solution 2

---
+++ Algorithms/Stack/[M] 155. Min Stack/sol2.py
@@ -0,0 +1,22 @@
+from collections import deque 
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.mins = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        cmin = self.mins[-1] if len(self.mins) > 0 else val
+        self.mins.append(min(val, cmin))
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.mins.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.mins[-1]
