2e994c92dedde1f976791d3dd0b4073c57e71ac6
Harry <harrylin349@gmail.com>
2025-02-05 02:25:55 -0500

    add Subsets (backtracking)

---
+++ Algorithms/Backtracking/[M] Subsets.py
@@ -0,0 +1,25 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        # idea: recursively
+        # backtracking, which is like DFS
+        # iterate through every num in nums
+        # for each, we either take in our curr subset or we don't
+        result = []
+        curSubset = []
+        n = len(nums)
+
+        def dfs(i):
+            # i >= n? we've evaluated every elem in nums, can return our decision here now
+            if i >= n:
+                result.append(curSubset.copy())
+                return
+            # choice 1
+            curSubset.append(nums[i])
+            dfs(i+1)
+
+            # choice 2
+            curSubset.pop()
+            dfs(i+1)
+            return
+        dfs(0)
+        return result
