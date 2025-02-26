e0ae771632bee9ab64e26b97b104c4863d3ddb33
Harry <harrylin349@gmail.com>
2025-02-26 15:45:41 -0500

    Permutations, Palindrome Partitioning

---
+++ Algorithms/Backtracking/[M] Palindrome Partitioning.py
@@ -0,0 +1,48 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        # all possible --> backtracking? 
+        # build cases as we go, don't discard any
+        
+        # idea: have a list[str] cur
+        # at each step, either:
+        # start a new string
+        # append to the previous string
+
+        # if our index == n, and each string in cur is a palindrome, add it
+
+        answers = []
+
+        def isPalindrome(string):
+            l,r = 0, len(string) - 1
+            while l <= r:
+                if string[l] != string[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+        
+        def backtrack(i, cur):
+
+            # stopping case
+            if (i == len(s)):
+                for word in cur:
+                    if not isPalindrome(word):
+                        return
+                answers.append(cur[:])
+                return
+            
+            # decisions
+            
+            # if cur word exists, add to existing
+            if (len(cur) > 0):
+                cur[-1] += s[i]
+                backtrack(i+1, cur)
+                cur[-1] = cur[-1][:-1]
+
+            # create a new word
+            cur.append(s[i])
+            backtrack(i+1, cur)
+            cur.pop()
+        
+        backtrack(0, [])
+        return answers
--- Algorithms/Backtracking/[M] Permutations.py
+++ Algorithms/Backtracking/[M] Permutations.py
@@ -21,3 +21,31 @@ class Solution:
         
         dfs(0)
         return result
+    
+'''
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        # take each elem of nums
+        # creating a permutation
+        # pick one of the elems
+            # remove it from the choices
+            # repeat?
+            # no duplicates (good)
+        # first idea --> backtracking ? 
+        
+        answers = []
+
+        def backtrack(cur):
+            if (len(cur) == len(nums)):
+                answers.append(cur[:])
+                return
+            for i in range(len(nums)):
+                if nums[i] not in cur: # O(n)
+                    cur.append(nums[i])
+                    backtrack(cur)
+                    cur.pop()
+            
+        backtrack([])
+        return answers
+
+'''
