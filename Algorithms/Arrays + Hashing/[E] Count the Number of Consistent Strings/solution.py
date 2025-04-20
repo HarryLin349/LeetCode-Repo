1460f5d998383c827c2df43545c33a7a280b1830
Harry <harrylin349@gmail.com>
2025-04-20 01:35:08 -0400

    Practice

---
+++ Algorithms/Arrays + Hashing/[E] Count the Number of Consistent Strings/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+        # idea: we just care about characters occuring
+        # not necessarily counts
+        allowSet = set(allowed)
+        res = 0
+        for word in words:
+            valid = True
+            for char in word:
+                if char not in allowSet:
+                    valid = False
+                    break
+            res += 1 if valid else 0
+        return res
