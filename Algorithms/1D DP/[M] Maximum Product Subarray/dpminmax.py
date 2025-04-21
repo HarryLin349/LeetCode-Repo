23652d29927f4eae3a73f592d5ed0595094ade46
Harry <harrylin349@gmail.com>
2025-04-21 03:02:55 -0400

    [M] Maximum Product Subarray

---
+++ Algorithms/1D DP/[M] Maximum Product Subarray/dpminmax.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        '''
+        idea
+        dp, dp[i] is the max product from i to n assuming we take i 
+        dp[i] is either nums[i] * dp[i+1], or nums[i] (assuming we don't take it)
+        '''
+        n = len(nums)
+        memo = [-1 for _ in range(n)]
+
+        def dp(i):
+            if i >= n:
+                return (1,1)
+            if (memo[i] != -1):
+                return memo[i]
+            maxp, minp = dp(i+1)
+            sol1, sol2 ,sol3 = nums[i], nums[i]  * maxp, nums[i] * minp
+            memo[i] = (max(sol1, sol2,sol3), min(sol1,sol2,sol3))
+            return memo[i]
+        dp(0)
+        maxproduct = nums[0]
+        for i in range(n):
+            maxproduct = max(maxproduct, memo[i][0])
+        return maxproduct
---
+++ Algorithms/1D DP/[M] Maximum Product Subarray/init.py
@@ -0,0 +1,36 @@
+# similar to kadane's algorithm
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        '''
+        idea
+        keep track of the current product
+        if product ever hits 0, reset (will never change)
+        if we hit 0 or an end, we divide each in the last reset to see if it improves
+        '''
+        cur = 1
+        lastzero = 0
+        maxproduct = nums[0]
+        n = len(nums)
+        for i in range(n):
+            print (f"at {i}, cur {cur}, max {maxproduct}, nums[{i}] = {nums[i]}")
+            if (nums[i] != 0):
+                cur *= nums[i]
+                maxproduct = max(maxproduct, cur)
+            print(f"cur now {cur}, max: {maxproduct}")
+            if nums[i] == 0 or i == n-1:
+                print("backtracking")
+                # we've reach an end, go back and check divides from the last zero
+                stopping = i-1 if nums[i] == 0 else i
+                for j in range(lastzero, stopping):
+                    print(f"==checking to divide out {j}: {nums[j]}")
+                    cur = cur // nums[j]
+                    maxproduct = max(maxproduct, cur)
+                # now we reset
+                lastzero = i + 1
+                cur = 1
+                if nums[i] == 0:
+                    maxproduct = max(maxproduct, 0)
+                    continue
+
+        return maxproduct
