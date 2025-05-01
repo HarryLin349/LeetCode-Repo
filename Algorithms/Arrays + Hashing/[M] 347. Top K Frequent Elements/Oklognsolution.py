2cd0ef6438cef48c29004a2e4cf0cfbbba729e5e
Harry <harrylin349@gmail.com>
2025-05-01 14:13:22 -0400

    [M] Top K freq elems

---
+++ Algorithms/Arrays + Hashing/[M] 347. Top K Frequent Elements/Oklognsolution.py
@@ -0,0 +1,23 @@
+from heapq import heappush, heappop, heapify
+from collections import defaultdict
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # problem:  find the most freq elems
+        # naive approach: dict of elems 
+        # then sort by seen and return first k
+
+        '''
+        idea: tuples of (num, occurence)
+        heapify and then return the top k
+        '''
+        seen = defaultdict(int)
+        for num in nums:
+            seen[num] += 1
+        lst = []
+        res = []
+        for key, val in seen.items():
+            lst.append((-val,key))
+        heapify(lst)
+        for i in range(k):
+            res.append(heappop(lst)[1])
+        return res
