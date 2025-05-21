bbb2273a50534f5fccf980cf29afc4ebb490322d
Harry <harrylin349@gmail.com>
2025-05-20 22:12:25 -0400

    set Matrix zeroes.py

---
+++ Algorithms/_ Dailys/[M] Set Matrix Zeroes.py
@@ -0,0 +1,23 @@
+'''
+Employs hashing strategies
+'''
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        naively, scan through once to get a set of zeroes
+        then, set the cols and rows with those to zero
+        """
+        zeroes = set()
+        n,m = len(matrix), len(matrix[0])
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] == 0:
+                    zeroes.add((i,j))
+        
+        for row, col in zeroes:
+            for i in range(n):
+                matrix[i][col] = 0
+            for j in range(m):
+                matrix[row][j] = 0
+        
