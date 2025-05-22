ce83808b04ff60b0afe9cf0c236b9a957f79b45a
Harry <harrylin349@gmail.com>
2025-05-22 14:14:30 -0400

    [M] Minimum Equqal Sum of Two Arrays

---
+++ Algorithms/_ Dailys/May_25/[M] Minimum Equal Sum of Two arrays.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minSum(self, nums1: List[int], nums2: List[int]) -> int:
+        '''
+        idea: greedily check
+        if we have any zeroes q, we can scale the array from >= n + q 
+        the min sum will be the max of n+q between the two
+        impossible case is if q = 0 for an arr and that arr is less than the others n+q
+        '''
+        sum1, sum2, z1, z2 = 0,0,0,0
+        for n in nums1:
+            sum1 += n
+            if n == 0:
+                z1+=1
+        for n in nums2:
+            sum2 += n
+            if n==0:
+                z2+=1
+        
+        if (z1 == 0 and sum1 < sum2 + z2):
+            return -1
+        if (z2 == 0 and sum2 < sum1 + z1):
+            return -1
+        return max(sum1+z1, sum2+z2)
