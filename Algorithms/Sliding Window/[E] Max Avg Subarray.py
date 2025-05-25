afe345794ea1194c90c96b698498caf0824707f2
Harry <harrylin349@gmail.com>
2025-05-25 12:50:59 -0400

    [E] Max avg Subarray I

---
+++ Algorithms/Sliding Window/[E] Max Avg Subarray.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        '''
+        ideas
+        we want a length equal to k
+        with the max average value
+
+        track max average w/ variable, maxavg
+
+        sliding window, just increment l and r by 1 each time
+        then divide by k at the end
+        '''
+        l, r = 0, k-1
+        curAvg = sum(nums[:k])
+        maxAvg = curAvg
+        for i in range(1, len(nums) - k + 1):
+            curAvg += nums[i+k-1]
+            curAvg -= nums[i-1]
+            maxAvg = max(maxAvg, curAvg)
+        return maxAvg / k
