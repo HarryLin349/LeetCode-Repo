1a85b1ddd3d99a7a24bc9d376e4bfef6913d81a5
Harry <harrylin349@gmail.com>
2025-05-25 23:17:56 -0400

    [M] Longest Subarray of 1's After Deleting One Element

---
+++ Algorithms/Sliding Window/[M] Longest Subarray of 1's After Deleting One Element.py
@@ -0,0 +1,31 @@
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        '''
+        idea: treat this as we have 1 "skip"
+        l,r is the cur subarray, l,r = 0
+        res, cur
+        for r in range(len(nums))
+            if r is 1, cur += 1
+            elif skip = 1
+                skip -= 1
+            else:
+                while l <= r and skip < 0:
+                    if nums[l] = 0:
+                        skip +=  1
+                    l += 1
+                    cur -= 1
+        '''
+        res, skip = 0,1
+        l = 0
+        for r in range(len(nums)):
+            if nums[r] == 0:
+                if skip == 1:
+                    skip -= 1
+                else:
+                    skip -=1
+                    while l <= r and skip < 0:
+                        if nums[l] == 0:
+                            skip += 1
+                        l += 1
+            res = max(res, r-l+1 - 1)
+        return res
