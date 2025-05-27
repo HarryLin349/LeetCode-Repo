b697c70609fc8ab927a9a8c81f34b006ef977eaa
Harry <harrylin349@gmail.com>
2025-05-26 20:54:43 -0400

    [E] Unique Num of Occurences

---
+++ Algorithms/Arrays + Hashing/[E] Unique Num of Occurences.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        counts = defaultdict(int)
+        seen = set()
+        for num in arr:
+            counts[num] += 1
+        for num, count in counts.items():
+            if count in seen:
+                return False
+            seen.add(count)
+        return True
